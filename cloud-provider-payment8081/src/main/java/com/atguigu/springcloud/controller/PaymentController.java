335500acb1ad5c6d6ab031cad74472a852213c5d
695954085 <695954085@qq.com>
2021-01-09 19:06:13 +0800

    1

---
+++ cloud-provider-payment8081/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/payment/create")
+    public CommonResult create(Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("insert result: {}", result);
+        if (result > 0) {
+            return new CommonResult<>(200, "insert successfully", result);
+        } else {
+            return new CommonResult<>(500, "insert fail", null);
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("payment: {}", payment);
+        if (payment != null) {
+            return new CommonResult<>(200, "query successfully", payment);
+        } else {
+            return new CommonResult<>(500, "query fail", id);
+        }
+    }
+}
