8a079d7d1a1a465ea2d72b687c003295cd5db685
695954085 <695954085@qq.com>
2021-01-10 21:49:46 +0800

    1

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public ResponseEntity<CommonResult> getPayment(@PathVariable("id") String id) {
+        return restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+}
