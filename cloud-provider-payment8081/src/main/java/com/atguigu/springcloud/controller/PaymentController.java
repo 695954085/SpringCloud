e9dbec5344784b2a0465af3c2e4b34e65d255bef
695954085 <695954085@qq.com>
2021-01-17 20:11:30 +0800

    1

--- cloud-provider-payment8081/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8081/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,7 +5,13 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Service;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -14,12 +20,18 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String port;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("insert result: {}", result);
         if (result > 0) {
-            return new CommonResult<>(200, "insert successfully", result);
+            return new CommonResult<>(200, "insert successfully, port: " + port, result);
         } else {
             return new CommonResult<>(500, "insert fail", null);
         }
@@ -30,9 +42,23 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("payment: {}", payment);
         if (payment != null) {
-            return new CommonResult<>(200, "query successfully", payment);
+            return new CommonResult<>(200, "query successfully, port: " + port, payment);
         } else {
             return new CommonResult<>(500, "query fail", id);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for(String element: services) {
+            log.info("**********element: " + element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance instance: instances) {
+            log.info("***********instance: " + instance.getInstanceId() + "  host:" + instance.getHost() + "  port: " + instance.getPort());
+        }
+        return discoveryClient;
     }
 }
