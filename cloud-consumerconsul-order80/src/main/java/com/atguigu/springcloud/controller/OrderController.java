dd7e812cfe37245c34980dee2e2c5f6c5a1fdd8b
695954085 <695954085@qq.com>
2021-01-19 20:12:14 +0800

    consul

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://cloud-payment-service";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/get/consul")
+    public String paymentInfo() {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/consul", String.class);
+    }
+
+
+}
